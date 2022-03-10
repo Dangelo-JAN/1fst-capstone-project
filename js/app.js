37a08b00fd5b88f18d62e7919a1beb505bdcb0ea
Dangelo Arrivillaga <talgidisoluciones@gmail.com>
2022-03-10 15:16:40 -0400

    End main section

--- js/app.js
+++ js/app.js
@@ -2,6 +2,47 @@ const hambIcon = document.querySelector('#hamb-icon');
 const navbarNav = document.querySelector('#navbarNav');
 const mainContainer = document.getElementById('main-container');
 const mainCard = document.getElementsByClassName('main-card');
+const team = {
+  'partners': [
+    {
+      'id': 01,
+      'name': 'partener01',
+      'tittle': 'tittle01',
+      'comment': 'comment01',
+      'photo':
+    },
+    {
+      'id': 02,
+      'name': 'partener02',
+      'tittle': 'tittle02',
+      'comment': 'comment02',
+    },
+    {
+      'id': 03,
+      'name': 'partener03',
+      'tittle': 'tittle03',
+      'comment': 'comment03',
+    },
+    {
+      'id': 04,
+      'name': 'partener04',
+      'tittle': 'tittle04',
+      'comment': 'comment04',
+    },
+    {
+      'id': 05,
+      'name': 'partener05',
+      'tittle': 'tittle05',
+      'comment': 'comment05',
+    },
+    {
+      'id': 06,
+      'name': 'partener06',
+      'tittle': 'tittle06',
+      'comment': 'comment06',
+    }
+    ]
+}
 let windowWidth = window.innerWidth;
 
 hambIcon.addEventListener('click', () => {
