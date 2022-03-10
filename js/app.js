24044b313ee4bf0139c1c3e90c80176e34ff2cb6
Dangelo Arrivillaga <talgidisoluciones@gmail.com>
2022-03-09 20:59:41 -0400

    Fix main section's resposive dynamically

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
 const hambIcon = document.querySelector('#hamb-icon');
 const navbarNav = document.querySelector('#navbarNav');
-const mainCard = document.querySelector('.main-card');
+const mainContainer = document.getElementById('main-container');
+const mainCard = document.getElementsByClassName('main-card');
+let windowWidth = window.innerWidth;
 
 hambIcon.addEventListener('click', () => {
     navbarNav.classList.toggle('mobile-menu');
@@ -13,13 +15,18 @@ document.querySelectorAll('.nav-item').forEach((n) => n.addEventListener('click'
     navbarNav.classList.toggle('show');
 }));
 
-if (screen.width < 768) {
-  mainCard.classList.replace('flex-column', 'flex-row');    
-} else
-    mainCard.classList.replace('flex-row', 'flex-column');
-/*
-if (window.innerWidth < 768) {
-  mainCard.classList.replace('flex-column', 'flex-row');    
-} else
-    mainCard.classList.replace('flex-row', 'flex-column');
-*/
+if (windowWidth < 768) {
+    mainContainer.classList.replace('flex-row', 'flex-column');
+    mainCard[0].classList.replace('flex-cloumn', 'flex-row');
+    mainCard[1].classList.replace('flex-cloumn', 'flex-row');
+    mainCard[2].classList.replace('flex-cloumn', 'flex-row');
+    mainCard[3].classList.replace('flex-cloumn', 'flex-row');
+    mainCard[4].classList.replace('flex-cloumn', 'flex-row');    
+} else {
+    mainContainer.classList.replace('flex-column', 'flex-row');
+    mainCard[0].classList.replace('flex-row', 'flex-column');
+    mainCard[1].classList.replace('flex-row', 'flex-column');
+    mainCard[2].classList.replace('flex-row', 'flex-column');
+    mainCard[3].classList.replace('flex-row', 'flex-column');
+    mainCard[4].classList.replace('flex-row', 'flex-column');
+}
